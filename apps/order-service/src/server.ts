354ba92e9ad04975468642cee2054766aaafc768
Hirzi Nurfakhrian <hirzinurfakhrian@gmail.com>
2024-12-16 09:36:43 +0700

    chore: using shared eslint

--- apps/order-service/src/server.ts
+++ apps/order-service/src/server.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './modules/app/app.module';
 
-async function bootstrap() {
+async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3100);
 }
-bootstrap();
+bootstrap().catch((err) => {
+  console.error('cannot start server', err);
+});
